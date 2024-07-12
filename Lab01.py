7ed1d551b73e98e5aca58d024f9198b0b62363f5
José Antonio Mérida <138615863+TonitoMC@users.noreply.github.com>
2024-07-11 22:31:16 -0600

    Add files via upload

---
+++ Lab01.py
@@ -0,0 +1,61 @@
+import pygame
+from pygame.locals import *
+from gl import Renderer
+
+#Variables de dimensiones de la pantalla
+width = 960
+height = 540
+#Inicializa la pantalla de Pygame
+screen = pygame.display.set_mode((width,height))
+
+#Refresh rate / FPS
+clock = pygame.time.Clock()
+
+#Loop donde todo va a estar corriendo
+isRunning = True
+
+rend = Renderer(screen)
+
+rend.glColor(1,0,1)
+rend.glClearColor(0,0,0)
+
+def poligono(listaPuntos):
+    for i in range(len(listaPuntos)):
+        rend.glLine(listaPuntos[i],listaPuntos[(i + 1)%len(listaPuntos)], (1,1,1))
+
+pol1 = [(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360),
+        (250, 380), (220, 385), (205, 410), (193, 383)]
+
+pol2 = [(321, 335), (288, 286), (339, 251), (374, 302)]
+
+pol3 = [(377, 249), (411, 197), (436, 249)]
+
+pol4 = [(413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52), (750, 145), (761, 179), (672, 192),
+        (659, 214), (615, 214), (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)]
+
+while isRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                rend.glGenerateFrameBuffer('bitmap.bmp')
+                isRunning = False
+    pygame.display.flip()
+    clock.tick(60)
+    rend.glClear()
+    rend.glFill(pol3, (1,1,1))
+    #poligono(pol1)
+    #poligono(pol2)
+    #poligono(pol3)
+    #poligono(pol4)
+
+    #for x in range(0, width, 10):
+    #   rend.glLine(punto0,(x, height), (0,0,0))
+    #for x in range(0, width, 10):
+    #    rend.glLine(punto0, (x, 0), (0,0,0))
+    #for x in range (0, height, 10):
+    #    rend.glLine(punto0, (0,x), (0,0,0))
+    #for x in range (0, height, 10):
+    #    rend.glLine(punto0, (width, x), (0, 0, 0))
+pygame.quit()
---
+++ gl.py
@@ -0,0 +1,201 @@
+import struct
+
+def char(c):
+    # 1 byte
+    return struct.pack("=c", c.encode("ascii"))
+
+def word(w):
+    # 2 bytes
+    return struct.pack("=h", w)
+
+def dword(d):
+    # 4 bytes
+    return struct.pack("=l", d)
+
+
+
+class Renderer(object):
+    def __init__(self, screen):
+        self.screen = screen
+        # No se toman las primeras dos, el screen.get_rect nos da las posiciones de origen y unicamente queremos width + height
+        _, _, self.width, self.height = screen.get_rect()
+
+        self.glColor(1, 1, 1)
+        self.glClearColor(0, 0, 0)
+        self.glClear()
+
+    def glGenerateFrameBuffer(self, filename):
+        with open(filename, "wb") as file:
+            # Header
+            file.write(char("B"))
+            file.write(char("M"))
+            file.write(dword(14 + 40 + (self.width * self.height * 3)))
+            file.write(dword(0))
+            file.write(dword(14 + 40))
+
+            # Info Header
+            file.write(dword(40))
+            file.write(dword(self.width))
+            file.write(dword(self.height))
+            file.write(word(1))
+            file.write(word(24))
+            file.write(dword(0))
+            file.write(dword(self.width * self.height * 3))
+            file.write(dword(0))
+            file.write(dword(0))
+            file.write(dword(0))
+            file.write(dword(0))
+
+            # Color table
+            for y in range(self.height):
+                for x in range(self.width):
+                    color = self.frameBuffer[x][y]
+                    color = bytes([color[2],
+                                   color[1],
+                                   color[0]])
+                    file.write(color)
+
+    def glColor(self, r, g, b):
+        r = min(1, max(0, r))
+        g = min(1, max(0, g))
+        b = min(1, max(0, b))
+
+        self.currColor = [r, g, b]
+
+    def glClearColor(self, r, g, b):
+        r = min(1, max(0, r))
+        g = min(1, max(0, g))
+        b = min(1, max(0, b))
+
+        self.clearColor = [r, g, b]
+
+    def glClear(self):
+        color = [int(i * 255) for i in self.clearColor]
+        self.screen.fill(color)
+
+        self.frameBuffer = [[self.clearColor for y in range(self.height)]
+                            for x in range(self.width)]
+
+    # Pygame dibuja desde superior izquierda
+    def glPoint(self, x, y, color=None):
+        # Pygame recibe colores de 0 a 255
+        if (0 <= x < self.width and (0 <= y < self.height)):
+            color = [int(i * 255) for i in (color or self.currColor)]
+            self.screen.set_at((x, self.height - 1 - y), color)
+            self.frameBuffer[x][y] = color
+
+    #Implementacion de scanline
+    def glFill(self, listaPuntos, color):
+        listaPuntosConEdges = [p for p in listaPuntos]
+        #Dibuja el poligono
+        for i in range(len(listaPuntos)):
+            listaPuntosConEdges += self.glLineWithPoints(listaPuntos[i], listaPuntos[(i + 1) % len(listaPuntos)], color)
+        #Encuentra las coordenadas
+        coords_x = [coord[0] for coord in listaPuntos]
+        coords_y = [coord[1] for coord in listaPuntos]
+
+        min_x = min(coords_x)
+        max_x = max(coords_x)
+        min_y = min(coords_y)
+        max_y = max(coords_y)
+        #Recorre las lineas en Y una por una, no se llena la linea de "tope" ni la linea de hasta abajo
+        for i in range(min_y + 1, max_y):
+            validCoords = []
+            validCoords += [coord for coord in listaPuntosConEdges if coord[1] == i]
+            for p in range(0, len(validCoords) - 1, 2):  # Increment p by 2 each time
+                self.glLine(validCoords[p], validCoords[p + 1], color)
+
+    #glLine pero retorna una lista de tuplas con los puntos que dibujo
+    def glLineWithPoints(self, v0, v1, color):
+        points = []
+        x0 = int(v0[0])
+        x1 = int(v1[0])
+        y0 = int(v0[1])
+        y1 = int(v1[1])
+
+        # Algoritmo de Lineas de Bresenham
+
+        if x0 == x1 and y0 == y1:
+            self.glPoint(x0, y0, color)
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        steep = dy > dx
+
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+
+        if x1 < x0:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        offset = 0
+        limit = 0.5
+        m = dy / dx
+        y = y0
+        for x in range(x0, x1 + 1):
+            if steep:
+                self.glPoint(y, x, color or self.currColor)
+                points.append((y,x))
+            else:
+                self.glPoint(x, y, color or self.currColor)
+                points.append((x,y))
+            offset += m
+            if offset >= limit:
+                if y0 < y1:
+                    y += 1
+                else:
+                    y -= 1
+                limit += 1
+        return points
+
+    def glLine(self, v0, v1, color):
+        x0 = int(v0[0])
+        x1 = int(v1[0])
+        y0 = int(v0[1])
+        y1 = int(v1[1])
+
+        # Algoritmo de Lineas de Bresenham
+
+        if x0 == x1 and y0 == y1:
+            self.glPoint(x0, y0, color)
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        steep = dy > dx
+
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+
+        if x1 < x0:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+
+        offset = 0
+        limit = 0.5
+        m = dy / dx
+        y = y0
+
+        for x in range(x0, x1 + 1):
+            if steep:
+                self.glPoint(y, x, color or self.currColor)
+            else:
+                self.glPoint(x, y, color or self.currColor)
+            offset += m
+            if offset >= limit:
+                if y0 < y1:
+                    y += 1
+                else:
+                    y -= 1
+                limit += 1
+
