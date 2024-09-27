47d1759e1187d54d1d6c33c73af992ef962b3c1f
José Antonio Mérida <joseameridac@gmail.com>
2024-09-26 23:46:10 -0600

    Add textures remove numpy

--- Raytracer2024.py
+++ Raytracer2024.py
@@ -7,8 +7,8 @@ from figures import *
 from material import *
 from lights import *
 from texture import *
-width = 800
-height = 450
+width = 400
+height = 225
 
 screen = pygame.display.set_mode((width, height), pygame.SCALED )
 clock = pygame.time.Clock()
@@ -18,9 +18,8 @@ rt.envMap = Texture("textures/lot.bmp")
 rt.glClearColor(0.5,0.0,0.0)
 rt.glClear()
 
-
-brick = Material(diffuse = [1.0,0.2,0.2], spec = 128, Ks = 0.25)
-grass = Material(diffuse = [0.2,1.0,0.2], spec = 64, Ks = 0.2)
+# brick = Material(diffuse = [1.0,0.2,0.2], spec = 128, Ks = 0.25)
+# grass = Material(diffuse = [0.2,1.0,0.2], spec = 64, Ks = 0.2)
 
 mirror = Material(diffuse = [0.9, 0.9, 0.9], spec = 128, Ks = 0.2, matType = REFLECTIVE)
 blueMirror = Material(diffuse = [0.5, 0.5, 1.0], spec = 128, Ks = 0.2, matType = REFLECTIVE)
@@ -30,21 +29,31 @@ glass = Material(spec = 128, Ks = 0.2, ior = 1.5, matType = TRANSPARENT)
 rt.lights.append( DirectionalLight(direction = [-1,-1,-1], intensity = 0.8))
 rt.lights.append( AmbientLight(intensity = 0.1))
 
-# Opacas - Izquierda
-rt.scene.append( Sphere(position = [-2.5, -1.25, -5], radius = 1, material = paint))
-rt.scene.append( Sphere(position = [-2.5, 1.25, -5], radius = 1, material = paint))
+brick = Material(spec = 128, Ks = 0.25, matType = OPAQUE, texture = Texture("textures/brick2.bmp"))
+pavement = Material(spec = 128, Ks = 0.25, matType = OPAQUE, texture = Texture("textures/pavement.bmp"))
+
+ice = Material(spec = 128, Ks = 0.2, matType = REFLECTIVE, texture = Texture("textures/ice.bmp"))
+marble = Material(spec = 128, Ks = 0.2, matType = REFLECTIVE, texture = Texture("textures/marble.bmp"))
+
+
+water = Material(spec = 128, Ks = 0.2, ior = 1.5, matType = TRANSPARENT, texture = Texture("textures/watersimple.bmp"))
+water = Material(spec = 128, Ks = 0.2, ior = 1.5, matType = TRANSPARENT, texture = Texture("textures/watersimple.bmp"))
+
+
+# # Opacas - Izquierda
+# rt.scene.append( Sphere(position = [-2.5, -1.25, -5], radius = 1, material = brick))
+# rt.scene.append( Sphere(position = [-2.5, 1.25, -5], radius = 1, material = pavement))
 
 # Reflectivas - Centro
-rt.scene.append( Sphere(position = [0, -1.25, -5], radius = 1, material = mirror))
-rt.scene.append( Sphere(position = [0, 1.25, -5], radius = 1, material = mirror))
+rt.scene.append( Sphere(position = [0, -1.25, -5], radius = 1, material = ice))
+rt.scene.append( Sphere(position = [0, 1.25, -5], radius = 1, material = marble))
 
 # Refractivas - Derecha
-rt.scene.append( Sphere(position = [2.5, -1.25, -5], radius = 1, material = glass))
-rt.scene.append( Sphere(position = [2.5, 1.25, -5], radius = 1, material = glass))
+rt.scene.append( Sphere(position = [2.5, -1.25, -5], radius = 1, material = water))
+rt.scene.append( Sphere(position = [2.5, 1.25, -5], radius = 1, material = water))
 
 # Dos adicionales para mostrar Refraccion / Refleccion
-rt.scene.append( Sphere(position = [2.5, 0, -7.5], radius = 0.5, material = paint))
-rt.scene.append( Sphere(position = [0, 0, -2.5], radius = 0.25, material = paint))
+rt.scene.append( Sphere(position = [2.5, -1.25, -7.5], radius = 0.5, material = paint))
 
 
 rt.glRender()
